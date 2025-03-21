5748545867cc84ad72d8a33b16c9ef0d642bed83
0xC0FFE2 <github1@ncloud.sbs>
2025-03-21 11:49:22 +0900

    feat : add animal domain spi

---
+++ application-core/src/main/kotlin/team/gram/aspismain/domain/animal/dto/AnimalResponse.kt
@@ -0,0 +1,35 @@
+package team.gram.aspismain.domain.animal.dto
+
+import team.gram.aspismain.domain.animal.model.Animal
+import team.gram.aspismain.domain.animal.model.AnimalSexType
+import team.gram.aspismain.domain.animal.model.AnimalStatusType
+import java.util.UUID
+import java.util.Date
+
+data class AnimalResponse(
+    val id: UUID,
+    val name: String,
+    val breedId: UUID,
+    val admissionDate: Date,
+    val sex: AnimalSexType,
+    val animalStatus: AnimalStatusType,
+    val profileUrl: String
+) {
+    companion object {
+        fun of(animal: Animal): AnimalResponse {
+            return AnimalResponse(
+                id = animal.id,
+                name = animal.name,
+                breedId = animal.breedId,
+                admissionDate = animal.admissionDate,
+                sex = animal.sex,
+                animalStatus = animal.animalStatus,
+                profileUrl = animal.profile_url
+            )
+        }
+    }
+}
+
+data class AnimalsResponse(
+    val animals: List<AnimalResponse>
+)
---
+++ application-core/src/main/kotlin/team/gram/aspismain/domain/animal/spi/AnimalPort.kt
@@ -0,0 +1,4 @@
+package team.gram.aspismain.domain.animal.spi
+
+interface AnimalPort {
+}
