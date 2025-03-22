518613574a040455b05cbc0164d1ce4b8024bf2d
0xC0FFE2 <github1@ncloud.sbs>
2025-03-23 05:34:18 +0900

    feat : add ErrorEntryPoint

--- application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/CustomAuthenticationEntryPoint.kt
+++ application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/CustomAuthenticationEntryPoint.kt
@@ -1,20 +1,36 @@
 package team.gram.aspismain.global.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
-import team.gram.aspismain.global.security.exception.ForbiddenException
+import team.gram.aspismain.global.security.exception.error.SecurityExceptionCode
 
 @Component
-class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper
+) : AuthenticationEntryPoint {
 
     override fun commence(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         authException: AuthenticationException?
     ) {
-        throw ForbiddenException
+        val errorCode = SecurityExceptionCode.FORBIDDEN_ACCESS
+
+        response?.status = errorCode.status()
+        response?.contentType = MediaType.APPLICATION_JSON_VALUE
+        response?.characterEncoding = "UTF-8"
+
+        val errorResponse = mapOf(
+            "status" to errorCode.status(),
+            "code" to errorCode.code(),
+            "message" to errorCode.message()
+        )
+
+        response?.writer?.write(objectMapper.writeValueAsString(errorResponse))
     }
 }
