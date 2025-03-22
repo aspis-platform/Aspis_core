b7adb1a79c544dbb4df973a67a0ab4faaea432a0
0xC0FFE2 <github1@ncloud.sbs>
2025-03-23 05:14:41 +0900

    fix : change SecurityProperties logic

--- application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/SecurityProperties.kt
+++ application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/SecurityProperties.kt
@@ -2,17 +2,21 @@ package team.gram.aspismain.global.security
 
 import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 import java.util.Base64
 import javax.crypto.SecretKey
 
+@Component
 @ConfigurationProperties(prefix = "secret")
-class SecurityProperties(
-    secretKey: String,
-    val accessExp: Int,
-    val refreshExp: Int,
-) {
-    val secretKey: SecretKey = Keys.hmacShaKeyFor(
-        Base64.getEncoder().encodeToString(secretKey.toByteArray())
-            .toByteArray(Charsets.UTF_8)
-    )
+class SecurityProperties {
+    lateinit var secretKey: String
+    lateinit var accessExp: Number
+    lateinit var refreshExp: Number
+
+    fun getSigningKey(): SecretKey {
+        return Keys.hmacShaKeyFor(
+            Base64.getEncoder().encodeToString(secretKey.toByteArray())
+                .toByteArray(Charsets.UTF_8)
+        )
+    }
 }
