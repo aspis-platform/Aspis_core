66ca1a633af10ec889a1cd34f0e149ab547d5acc
0xC0FFE2 <github1@ncloud.sbs>
2025-03-23 05:14:55 +0900

    fix : add component annotation in @usecase

--- application-core/src/main/kotlin/team/gram/aspismain/common/annotation/UseCase.kt
+++ application-core/src/main/kotlin/team/gram/aspismain/common/annotation/UseCase.kt
@@ -1,8 +1,10 @@
 package team.gram.aspismain.common.annotation
 
+import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @Transactional
+@Component
 annotation class UseCase
