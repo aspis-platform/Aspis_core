97d44af913d01a4046ca06f1533f03f94d28d6dd
0xC0FFE2 <github1@ncloud.sbs>
2025-03-23 05:34:10 +0900

    feat : add corsConfig

--- application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/CorsConfig.kt
+++ application-infrastructure/src/main/kotlin/team/gram/aspismain/global/security/CorsConfig.kt
@@ -9,28 +9,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 @Configuration
 class CorsConfig {
 
-//    @Bean
-//    fun corsConfigurationSource(): CorsConfigurationSource {
-//        val configuration = CorsConfiguration()
-//
-//        configuration.allowedOrigins = listOf("**")
-//        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-//        configuration.allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
-//        configuration.allowCredentials = true
-//        configuration.maxAge = 3600L
-//
-//        val source = UrlBasedCorsConfigurationSource()
-//        source.registerCorsConfiguration("/**", configuration)
-//        return source
-//    }
-
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
 
-        configuration.allowedOrigins = listOf("*")
-        // 또는 allowedOriginPatterns 사용
-        // configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedOrigins = listOf("http://localhost:3000","http://localhost:5173","http://localhost:5174","https://aspis-staging.ncloud.sbs","https://aspis.ncloud.sbs")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         configuration.allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
         configuration.allowCredentials = true
@@ -40,4 +23,5 @@ class CorsConfig {
         source.registerCorsConfiguration("/**", configuration)
         return source
     }
+
 }
