6e325076d4b82449696793421cdf0aaad793d721
0xC0FFE2 <github1@ncloud.sbs>
2025-03-23 05:10:43 +0900

    chore : add scan config

--- application-infrastructure/src/main/kotlin/team/gram/aspismain/AspisMainServerApplication.kt
+++ application-infrastructure/src/main/kotlin/team/gram/aspismain/AspisMainServerApplication.kt
@@ -1,8 +1,14 @@
 package team.gram.team.gram.aspismain
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@ComponentScan(basePackages = ["team.gram"])
+@EnableJpaRepositories(basePackages = ["team.gram.aspismain.persistence"])
+@EntityScan(basePackages = ["team.gram.aspismain.persistence"])
 @SpringBootApplication
 class AspisMainServerApplication
 
